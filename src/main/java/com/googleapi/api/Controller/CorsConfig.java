8892d9d070313db4ac58bbacef3fed9a8e338004
tusharPanchalAppSphere <150894260+tusharPanchalAppSphere@users.noreply.github.com>
2024-04-03 14:24:39 +0530

    Update CorsConfig.java

--- src/main/java/com/googleapi/api/Controller/CorsConfig.java
+++ src/main/java/com/googleapi/api/Controller/CorsConfig.java
@@ -1,17 +1,17 @@
-//package com.googleapi.api.Controller;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOriginPatterns("*") // Specify allowed origin
-//                .allowedMethods("GET", "POST", "PUT", "DELETE")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-//}
+package com.googleapi.api.Controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+   @Override
+   public void addCorsMappings(CorsRegistry registry) {
+       registry.addMapping("/**")
+               .allowedOriginPatterns("*") // Specify allowed origin
+               .allowedMethods("GET", "POST", "PUT", "DELETE")
+               .allowedHeaders("*")
+               .allowCredentials(true);
+   }
+}
