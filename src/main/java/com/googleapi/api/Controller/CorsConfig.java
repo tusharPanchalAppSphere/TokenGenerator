896c537275ea22a9159870475645244b14e0e7a5
tusharPanchalAppSphere <150894260+tusharPanchalAppSphere@users.noreply.github.com>
2024-04-03 14:30:22 +0530

    Update CorsConfig.java

--- src/main/java/com/googleapi/api/Controller/CorsConfig.java
+++ src/main/java/com/googleapi/api/Controller/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
    @Override
    public void addCorsMappings(CorsRegistry registry) {
        registry.addMapping("/**")
-               .allowedOriginPatterns("*") // Specify allowed origin
+               .allowedOrigins("*") // Specify allowed origin
                .allowedMethods("GET", "POST", "PUT", "DELETE")
                .allowedHeaders("*")
                .allowCredentials(true);
